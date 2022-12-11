f153d182c17f9a2bfb70f63da165038164e47ad5
Cezary Szymański <cezary.szymanski@deravesoftware.com>
2022-12-11 10:43:54 +0100

    Added amimation

--- components/Hero.vue
+++ components/Hero.vue
@@ -9,8 +9,46 @@
     <span class="hero__text">
       Contact me: <a href="mailto:cezszym@protonmail.com">cezszym@protonmail.com</a>
     </span>
+    <div class="hero__dots">
+      <div v-for="dot in dots" :key="dot" class="hero__dot" :class="`hero__dot--${dot}`" />
+    
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { gsap } from "gsap";
+import { useWindowSize } from '@vueuse/core'
+
+export default defineComponent({
+
+  name: "Hero",
+
+  setup() {
+
+    const { width: screenWidth, height: screenHeight } = useWindowSize()
+
+    const dots = Array.from({ length: 100}).map((el, index) => index)
+
+
+    onMounted(() => {
+      dots.forEach((dot) => {
+
+        const sign = dot % 2 === 0 ? 1 : -1
+
+        gsap.to(`.hero__dot--${dot}`, {y: sign * screenHeight.value / 4 , duration: 1, opacity: 0, repeat: -1, yoyo: true, delay: dot / 30  })
+      })
+    })
+
+    return {
+      screenWidth,
+      dots
+    }
+  }
+  
+})
+
+</script>
 
 <style scoped lang="scss">
 .hero {
@@ -20,15 +58,16 @@
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 2rem;
   font-size: 1.5rem;
   line-height: 1.2;
+  overflow-x: hidden;
   &__name {
     font-weight: 600;
     font-size: 2em;
   }
   &__text {
     display: block;
+    margin: 1rem 0;
   }
   @media(max-width: 1000px) {
     align-items: flex-start;
@@ -40,5 +79,17 @@
       display: block;
     }
   }
+  &__dots {
+    display: flex;
+    margin-top: 0.2rem;
+  }
+  &__dot {
+   height: 10px;
+   width: 10px;
+   background: var(--primary);
+   margin-right: 2px;
+   border-radius: 50%;
+  }
 }
+
 </style>
